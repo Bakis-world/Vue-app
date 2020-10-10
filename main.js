e679a96d4bbbd3170235fb0d8c895aeeb11262f4
Bakis-world <bakisola4life@gmail.com>
2020-10-09 22:44:52 -0700

    updated

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-Vue.component("product",{
-    props:{
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template: `
+var eventBus= new Vue()
+
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template: `
     	<div class="product">
 			<div class="product-image">
 				<img v-bind:src="image">
@@ -37,85 +39,89 @@ Vue.component("product",{
                 <button v-on:click="removeFromCart">Remove From Cart</button>
             </div>
             </div>
-            <div>
-                <h2>Reviews</h2>
-                <p>There are no reviews yet.</p>
-                <ul>
-                <li v-for="i in reviews">
-                    <p>{{ i.name }}</p>
-                    <p>Rating: {{ i.rating }}</p>
-                    <p>{{ i.review }}</p>
-                </li>
-            </div>
-            <product-review @review-submitted="addReview"></product-review>
+            
+                <product-tabs :reviews="reviews"></product-tabs>
+
+                       
+        
+           
+
         </div>
     `,
-    data() {
-        return {
-            brand: "Vue Mastery",
-            product: "Socks",
-            selectedVariant: 0,
-            onSale: true,
-            details: ["80% cotton", "20% polyester", "Gender-neutral"],
-            sizes: ["20 cm", "15 cm", "30 cm"],
-            link: "https://www.google.com",
-            variant: [
-                {
-                    variantId: 2234,
-                    variantColor: "green",
-                    variantImage: "./image/vmSocks-green-onWhite.jpg",
-                    variantQuantity: 10
-                },
-                {
-                    variantId: 2235,
-                    variantColor: "blue",
-                    variantImage: "./image/socks_blue.jpg",
-                    variantQuantity: 0
-                }
-            ],
-            reviews:[]
-
-        }
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Socks",
+      selectedVariant: 0,
+      onSale: true,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      sizes: ["20 cm", "15 cm", "30 cm"],
+      link: "https://www.google.com",
+      variant: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./image/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./image/socks_blue.jpg",
+          variantQuantity: 0,
+        },
+      ],
+      reviews: []
+    }
+  },
+  methods: {
+    addToCart: function () {
+      this.$emit("add-to-cart", this.variant[this.selectedVariant].variantId);
     },
-    methods: {
-        addToCart: function () {
-            this.$emit("add-to-cart", this.variant[this.selectedVariant].variantId)
-        },
-        removeFromCart: function () {
-            this.$emit("remove-from-cart", this.variant[this.selectedVariant].variantId)
-        },
-        updateProduct: function (index) {
-            this.selectedVariant = index
-        },
-        addReview (productReview){
-            this.reviews.push(productReview)
-        }
+    removeFromCart: function () {
+      this.$emit(
+        "remove-from-cart",
+        this.variant[this.selectedVariant].variantId
+      )
     },
-    computed: {
-        title() {
-            return this.brand + " " + this.product
-        },
-        image() {
-            return this.variant[this.selectedVariant].variantImage
-        },
-        inStock() {
-            return this.variant[this.selectedVariant].variantQuantity
-        },
-        shipping() {
-            if (this.premium) {
-                return "Free"
-            }else
-            return 2.99
-        }
-
+    updateProduct: function (index) {
+      this.selectedVariant = index;
     }
 
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variant[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variant[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else return 2.99;
+    }
+  },
+  mounted() {
+      eventBus.$on("review-submitted",productReview =>{
+           this.reviews.push(productReview)
+      })
+  }
 })
 
 Vue.component("product-review",{
     template:`
 
     <form class="review-form" @submit.prevent="onSubmit">
+
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b></p>
+    <ul>
+        <li v-for="i in errors">{{ i }}</li>
+    </ul>
 
     <p>
         <label for="name">Name:</label>
@@ -148,23 +154,70 @@ Vue.component("product-review",{
         return{
             name:null,
             review:null,
-            rating:null
+            rating:null,
+            errors: []
         }
     },
     methods:{
         onSubmit() {
-            let productReview = {
+            if(this.name && this.review && this.rating){
+              let productReview = {
                 name: this.name,
                 review: this.review,
-                rating: this.rating
+                rating: this.rating,
+              };
+              eventBus.$emit("review-submitted", productReview);
+              this.name = null;
+              this.review = null;
+              this.rating = null;
+              this.errors =[];
+            }else{
+                if(!this.name) this.errors.push("Name is required")
+                if (!this.rating) this.errors.push("Rating is required")
+                if (!this.review) this.errors.push("Review is required")
             }
-                this.$emit("review-submitted",productReview)
-                this.name= null
-                this.review= null 
-                this.rating= null
         }
     }
 })
+
+Vue.component("product-tabs", {
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+        <div>
+            <span class="tab" 
+             :class ="{activeTab: selectedTab === i}"
+             v-for ="(i, index) in tabs" 
+             :key ="index"
+             @click ="selectedTab = i">
+             {{ i }}</span>
+
+             <div v-show ="selectedTab ==='Reviews'">
+                <p v-if = "!reviews.length">There are no reviews yet.</p>
+                <ul>
+                <li v-for ="i in reviews">
+                    <p>{{ i.name }}</p>
+                    <p>Rating: {{ i.rating }}</p>
+                    <p>{{ i.review }}</p>
+                </li>
+                </ul>
+            </div>
+             <product-review  v-show ="selectedTab ==='Make a Review'" ></product-review>
+        </div>
+
+    `,
+  data() {
+    return {
+      tabs: ["Reviews", "Make a Review"],
+      selectedTab: "Reviews",
+    }
+  }
+})
+
 
 var app =new Vue({
     el : "#app",
